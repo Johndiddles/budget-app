4e31b434de01ce9338fb5db060ddb433db2f972c
Johndiddles <johnadepelumi@gmail.com>
2022-01-15 12:54:01 +0000

    initial commit upon completing the budget app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import { Button, Stack } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import BudgetCard from './components/BudgetCard.js';
+import AddBudgetModal from './components/AddBudgetModal.js';
+import ViewExpensesModal from './components/ViewExpensesModal.js';
+import AddExpenseModal from './components/AddExpenseModal.js';
+import UncategorizedBudgetCard from './components/UncategorizedBudgetCard'
+import TotalBudgetCard from './components/TotalBudgetCard'
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from './contexts/BudgetsContext';
+import './App.css'
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal] = useState(false);
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
+  const [viewExpensesModalBudgetId, setViewExpensesModalBudgetId] = useState()
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
+  const { budgets, getBudgetExpenses } = useBudgets()
+
+  function openAddExpenseModal (budgetId){
+    setShowAddExpenseModal(true);
+    setAddExpenseModalBudgetId(budgetId)
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainBody">
+    <Container className="" >
+      <Stack direction='horizontal' gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant='success' onClick={()=>setShowAddBudgetModal(true)}>Add Budget</Button>
+        <Button variant="outline-success" onClick={openAddExpenseModal}>Add Expense</Button>
+      </Stack>
+
+      <div style={{display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start",}}>
+
+        {
+          budgets.map(budget => 
+            {
+              const amount = getBudgetExpenses(budget.id).reduce((total, expense) => total + expense.amount, 0)
+            return (
+              <BudgetCard 
+              key={budget.id} 
+              name={budget.name} 
+              amount={amount} 
+              max={budget.max} 
+              onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+              onViewExpensesClick={() => setViewExpensesModalBudgetId(budget.id)}
+              />
+            )}
+          )
+        }
+        <UncategorizedBudgetCard onAddExpenseClick={openAddExpenseModal} onViewExpensesClick={() => setViewExpensesModalBudgetId(UNCATEGORIZED_BUDGET_ID)}/>
+        <TotalBudgetCard />
+      </div>
+
+    </Container>
+
+    <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddBudgetModal(false)}/>
+    <AddExpenseModal show={showAddExpenseModal} defaultBudgetId={addExpenseModalBudgetId} handleClose={() => setShowAddExpenseModal(false)}/>
+    <ViewExpensesModal budgetId={viewExpensesModalBudgetId} handleClose={() => setViewExpensesModalBudgetId()}/>
+
     </div>
   );
 }
---
+++ src/contexts/BudgetsContext.js
@@ -0,0 +1,63 @@
+import React, {useContext} from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import useLocalStorage from '../hooks/useLocalStorage.js'
+
+const BudgetsContext = React.createContext()
+
+export const UNCATEGORIZED_BUDGET_ID = "Uncategorized"
+
+export function useBudgets(){
+    return useContext(BudgetsContext)
+}
+
+export const BudgetsProvider = ({ children })=>{
+    const [budgets, setBudgets] = useLocalStorage("budgets", [])
+    const [expenses, setExpenses] = useLocalStorage("expense", [])
+
+    function getBudgetExpenses(budgetId){
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+    function addExpense({description, amount, budgetId }){
+        setExpenses(prevExpenses => {
+            return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
+        })
+    }
+    function addBudget({name, max}){
+        setBudgets(prevBudgets => {
+            if(prevBudgets.find(budget => budget.name === name)){
+                return prevBudgets
+            }
+            return [...prevBudgets, { id: uuidV4(), name, max}]
+        })
+    }
+    function deleteExpense({ id }){
+        //TODO: Deal with expenses
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(budget => budget.id !== id)
+        })
+    }
+    function deleteBudget({ id }) {
+        setExpenses(prevExpenses => {
+            return prevExpenses.map(expense => {
+                if (expense.budgetId !== id) return expense
+                return { ...expense, budgetId: UNCATEGORIZED_BUDGET_ID}
+            })
+        })
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+    }
+
+
+    return <BudgetsContext.Provider value={{
+        budgets,
+        expenses,
+        getBudgetExpenses,
+        addExpense,
+        addBudget,
+        deleteExpense,
+        deleteBudget
+    }}>
+        {children}
+    </BudgetsContext.Provider>
+}
